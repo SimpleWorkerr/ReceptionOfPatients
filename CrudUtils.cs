99bc958841a77efdb1cb00a01e59e74b95940c03
Zakhar Merkurev <merkurevzahar@gmail.com>
2023-10-21 23:12:33 +0800

    Вместо множества AppContext используется один
    Создавние общего объекта типа AppContext, который
    должен передавать во все сервисы через параметр

---
+++ CrudUtils.cs
@@ -0,0 +1,25 @@
+﻿namespace ReceptionOfPatients
+{
+    public class CrudUtils : ICRUDUtils
+    {
+        public void CreateData(AppContext appContext)
+        {
+            //Реализация добавления
+        }
+
+        public void DeleteData(AppContext appContext)
+        {
+            //Реализация удаления
+        }
+
+        public void ReadData(AppContext appContext)
+        {
+            //Реализация чтения
+        }
+
+        public void UpdateData(AppContext appContext)
+        {
+            //Реализация обновления
+        }
+    }
+}
---
+++ ICRUDUtils.cs
@@ -0,0 +1,11 @@
+﻿namespace ReceptionOfPatients
+{
+    public interface ICRUDUtils
+    {
+        public void CreateData(AppContext appContext);
+        public void ReadData(AppContext appContext);
+        public void UpdateData(AppContext appContext);
+        public void DeleteData(AppContext appContext);
+
+    }
+}
