36f2dd8594b6f0b23896ca554c79b2d0cd86821d
Zakhar Merkurev <merkurevzahar@gmail.com>
2023-10-23 10:33:23 +0800

    Создание класса(сервиса) для CRUD докторов
    Данный класс представляет сервис, реализующих интерфейс
    ICRUDUtils. Этот класс позволит осущеслять добавление,
    удаление, чтение, обнавление данных в таблице Doctors в
    баззе данных.

---
+++ CrudDoctorServices.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ReceptionOfPatients
+{
+    public class CrudDoctorServices : ICRUDUtilitis<Doctor>
+    {
+        public void CreateRange(AppContext context, IEnumerable<Doctor>? values)
+        {
+            if (values != null)
+            {
+
+                context.Doctors.AddRange(values);
+
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteRange(AppContext context, IEnumerable<int>? indexes)
+        {
+            if (indexes != null)
+            {
+
+                foreach (int index in indexes)
+                    context.Doctors.Remove(context.Doctors.First(doc => doc.Id == index));
+
+                context.SaveChanges();
+            }
+        }
+
+        public List<Doctor> Read(AppContext context)
+        {
+            var tempValue = context.Doctors.Include(doc => doc.Services);
+
+            return tempValue.ToList();
+        }
+
+        public void UpdateRange(AppContext context, IEnumerable<Doctor>? values)
+        {
+            if (values != null)
+            {
+                context.Doctors.UpdateRange(values);
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
