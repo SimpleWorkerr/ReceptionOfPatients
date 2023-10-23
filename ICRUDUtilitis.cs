0540467419c631bb6f0b91bc40ac1a52df985bf3
Zakhar Merkurev <merkurevzahar@gmail.com>
2023-10-23 10:30:05 +0800

    создани интерфейса для сервисов CRUD
    Создан интерфейс, классы реализующие который будут иметь возможность
    применять операции CRUD к базе данных.
    C - Create
    R - Read
    U - Update
    D - Delete

---
+++ ICRUDUtilitis.cs
@@ -0,0 +1,13 @@
+﻿namespace ReceptionOfPatients
+{
+    public interface ICRUDUtilitis<T>
+    {
+        public void CreateRange(AppContext context, IEnumerable<Doctor> values);
+        
+        public void UpdateRange(AppContext context, IEnumerable<Doctor> values);
+        
+        public void DeleteRange(AppContext context, IEnumerable<int> indexes);
+
+        public List<Doctor> Read(AppContext context);
+    }
+}
