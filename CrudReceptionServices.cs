35ff587cae47932a9c723a2551f6283b43de2073
Zakhar Merkurev <merkurevzahar@gmail.com>
2023-10-31 16:19:33 +0800

    Добавил CRUD для таблицы приём
    Чтение
    Добавление
    Удаление
    Изменение
    
    Завершение
    Также реализовано создание
    новой таблицы "Результат приёма" при
    завершении текущей

---
+++ CrudReceptionServices.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReceptionOfPatients
+{
+    public class CrudReceptionServices : ICRUDUtilitis<Reception>
+    {
+        public void CreateRange(AppContext context, IEnumerable<Reception>? values)
+        {
+            if (values != null)
+            {
+
+                context.Receptions.AddRange(values);
+
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteRange(AppContext context, IEnumerable<int>? indexes)
+        {
+            if (indexes != null)
+            {
+
+                foreach (int index in indexes)
+                    context.Receptions.Remove(context.Receptions.First(rec => rec.Id == index));
+
+                context.SaveChanges();
+            }
+        }
+        public void FinishRange(AppContext context, IEnumerable<ReceptionResult>? values)
+        {
+            if (values != null)
+            {
+                context.ReceptionResults.AddRange(values);
+
+                context.SaveChanges();
+            }
+        }
+
+        public List<Reception> Read(AppContext context)
+        {
+            var tempValue = context.Receptions.Include(rec => rec.Doctor).Include(rec => rec.Patient);
+
+            return tempValue.ToList();
+        }
+
+        public void UpdateRange(AppContext context, IEnumerable<Reception>? values)
+        {
+            if (values != null)
+            {
+                context.Receptions.UpdateRange(values);
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
---
+++ ReceptionMiddleware.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Text.Unicode;
+
+namespace ReceptionOfPatients
+{
+    public class ReceptionMiddleware
+    {
+        private RequestDelegate _next;
+        private AppContext _appContext;
+
+        public ReceptionMiddleware(RequestDelegate next, AppContext appContext)
+        {
+            _next = next;
+            _appContext = appContext;
+        }
+
+        public async Task InvokeAsync(HttpContext context, CrudReceptionServices services)
+        {
+            HttpResponse response = context.Response;
+            HttpRequest request = context.Request;
+
+            if (request.Path == "/reception")
+            {
+                var operation = request.Query["operation"];
+
+                switch (operation)
+                {
+                    case "create":
+
+
+                        services.CreateRange(_appContext, await request.ReadFromJsonAsync<List<Reception>>());
+
+                        break;
+
+                    case "update":
+
+                        services.UpdateRange(_appContext, await request.ReadFromJsonAsync<List<Reception>>());
+
+                        break;
+
+                    case "finish":
+
+                        services.FinishRange(_appContext, await request.ReadFromJsonAsync<List<ReceptionResult>>());
+
+                        break;
+
+                    case "delete":
+
+                        services.DeleteRange(_appContext, await request.ReadFromJsonAsync<List<int>>());
+
+                        break;
+
+                    case "read":
+                        JsonSerializerOptions options = new()
+                        {
+                            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                            ReferenceHandler = ReferenceHandler.IgnoreCycles,
+                            WriteIndented = true,
+
+                        };
+                        await response.WriteAsJsonAsync(services.Read(_appContext), options);
+
+                        break;
+
+                    default:
+                        await response.WriteAsJsonAsync(new ErrorClass() { Name = "Reception error", Description = "Unexpected operation" });
+                        break;
+                }
+            }
+            else
+                await _next.Invoke(context);
+        }
+    }
+}
