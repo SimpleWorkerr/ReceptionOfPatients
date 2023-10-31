72cd9f6d8b00dac20417f3eacc9484f9c8f0ceb4
Zakhar Merkurev <merkurevzahar@gmail.com>
2023-10-31 16:23:41 +0800

    Добавил CRUD для "Сервисы"
    Добавление
    Изменение
    Чтение
    Удаление

---
+++ CrudServiceServices.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReceptionOfPatients
+{
+    public class CrudServiceServices : ICRUDUtilitis<Service>
+    {
+        public void CreateRange(AppContext context, IEnumerable<Service>? values)
+        {
+            if (values != null)
+            {
+
+                context.Services.AddRange(values);
+
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteRange(AppContext context, IEnumerable<int>? indexes)
+        {
+            if (indexes != null)
+            {
+
+                foreach (int index in indexes)
+                    context.Services.Remove(context.Services.First(ser => ser.Id == index));
+
+                context.SaveChanges();
+            }
+        }
+
+        public List<Service> Read(AppContext context)
+        {
+            var tempValue = context.Services.Include(ser => ser.Doctor);
+
+            return tempValue.ToList();
+        }
+
+        public void UpdateRange(AppContext context, IEnumerable<Service>? values)
+        {
+            if (values != null)
+            {
+                context.Services.UpdateRange(values);
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
---
+++ ServiceMiddleware.cs
@@ -0,0 +1,70 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Text.Unicode;
+
+namespace ReceptionOfPatients
+{
+    public class ServiceMiddleware
+    {
+        private RequestDelegate _next;
+        private AppContext _appContext;
+
+        public ServiceMiddleware(RequestDelegate next, AppContext appContext)
+        {
+            _next = next;
+            _appContext = appContext;
+        }
+
+        public async Task InvokeAsync(HttpContext context, CrudServiceServices services)
+        {
+            HttpResponse response = context.Response;
+            HttpRequest request = context.Request;
+
+            if (request.Path == "/service")
+            {
+                var operation = request.Query["operation"];
+
+                switch (operation)
+                {
+                    case "create":
+
+
+                        services.CreateRange(_appContext, await request.ReadFromJsonAsync<List<Service>>());
+
+                        break;
+
+                    case "update":
+
+                        services.UpdateRange(_appContext, await request.ReadFromJsonAsync<List<Service>>());
+
+                        break;
+
+                    case "delete":
+
+                        services.DeleteRange(_appContext, await request.ReadFromJsonAsync<List<int>>());
+
+                        break;
+
+                    case "read":
+                        JsonSerializerOptions options = new()
+                        {
+                            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                            ReferenceHandler = ReferenceHandler.IgnoreCycles,
+                            WriteIndented = true,
+
+                        };
+                        await response.WriteAsJsonAsync(services.Read(_appContext), options);
+
+                        break;
+
+                    default:
+                        await response.WriteAsJsonAsync(new ErrorClass() { Name = "Service error", Description = "Unexpected operation" });
+                        break;
+                }
+            }
+            else
+                await _next.Invoke(context);
+        }
+    }
+}
