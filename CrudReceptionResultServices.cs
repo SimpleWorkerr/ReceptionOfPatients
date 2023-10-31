b7f5b397bb172247e2c1855ebadc8dc4e2a17bc9
Zakhar Merkurev <merkurevzahar@gmail.com>
2023-10-31 16:22:30 +0800

    Реализовал CRUD для "Результат приёма"
    Изменение
    Чтение
    Удаление

---
+++ CrudReceptionResultServices.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReceptionOfPatients
+{
+    public class CrudReceptionResultServices:ICRUDUtilitis<ReceptionResult>
+    {
+        public void CreateRange(AppContext context, IEnumerable<ReceptionResult>? values)
+        {
+            throw new NotImplementedException("Такая операция не подразумевается");
+        }
+
+        public void DeleteRange(AppContext context, IEnumerable<int>? indexes)
+        {
+            if (indexes != null)
+            {
+
+                foreach (int index in indexes)
+                    context.ReceptionResults.Remove(context.ReceptionResults.First(ser => ser.Id == index));
+
+                context.SaveChanges();
+            }
+        }
+
+        public List<ReceptionResult> Read(AppContext context)
+        {
+            var tempValue = context.ReceptionResults;
+
+            return tempValue.ToList();
+        }
+
+        public void UpdateRange(AppContext context, IEnumerable<ReceptionResult>? values)
+        {
+            if (values != null)
+            {
+                context.ReceptionResults.UpdateRange(values);
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
---
+++ ReceptionResultMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Text.Unicode;
+
+namespace ReceptionOfPatients
+{
+    public class ReceptionResultMiddleware
+    {
+        private RequestDelegate _next;
+        private AppContext _appContext;
+
+        public ReceptionResultMiddleware(RequestDelegate next, AppContext appContext)
+        {
+            _next = next;
+            _appContext = appContext;
+        }
+
+        public async Task InvokeAsync(HttpContext context, CrudReceptionResultServices services)
+        {
+            HttpResponse response = context.Response;
+            HttpRequest request = context.Request;
+
+            if (request.Path == "/receptionResult")
+            {
+                var operation = request.Query["operation"];
+
+                switch (operation)
+                {
+                    case "update":
+
+                        services.UpdateRange(_appContext, await request.ReadFromJsonAsync<List<ReceptionResult>>());
+
+                        break;
+
+                    case "delete":
+
+                        services.DeleteRange(_appContext, await request.ReadFromJsonAsync<List<int>>());
+
+                        break;
+
+                    case "read":
+                        JsonSerializerOptions options = new()
+                        {
+                            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                            ReferenceHandler = ReferenceHandler.IgnoreCycles,
+                            WriteIndented = true,
+
+                        };
+                        await response.WriteAsJsonAsync(services.Read(_appContext), options);
+
+                        break;
+
+                    default:
+                        await response.WriteAsJsonAsync(new ErrorClass() { Name = "ReceptionResult error", Description = "Unexpected operation" });
+                        break;
+                }
+            }
+            else
+                await _next.Invoke(context);
+        }
+    }
+}
