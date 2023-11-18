b8c36a41abf57bfa95fd086165917cad662d74ff
Zakhar Merkurev <merkurevzahar@gmail.com>
2023-11-18 20:45:03 +0800

    Добавление загрузки json о пациентах

---
+++ wwwroot/js/app-patient.js
@@ -0,0 +1,14 @@
+//Загрузка данных на страницу
+loadData();
+
+
+//Функция получения данных с сервера при загрузке страницы
+//Тажке это main функция, для загрузки всех данных
+async function loadData(){
+  const response = await fetch("patient?operation=read", {
+    method: "post",
+    headers: { "Accept": "application/json", "Content-Type": "application/json" }
+  });
+
+  console.log(response.json());
+}
