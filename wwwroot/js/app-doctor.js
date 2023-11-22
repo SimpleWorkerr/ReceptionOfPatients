5f8971cec2dbbef2a62dc10a2be73fb44096220e
Zakhar Merkurev <merkurevzahar@gmail.com>
2023-11-22 09:13:16 +0800

    фикс

--- wwwroot/js/app-doctor.js
+++ wwwroot/js/app-doctor.js
@@ -1,3 +1,5 @@
+//Общая переменная
+let responseDoctors;
 //Загрузка данных на страницу
 loadData();
 
@@ -9,16 +11,16 @@ function saveData() {
 //Функция получения данных с сервера при загрузке страницы
 //Тажке это main функция, для загрузки всех данных
 async function loadData() {
-    const response = await fetch("doctor?operation=read", {
+    responseDoctors = await fetch("doctor?operation=read", {
         method: "post",
         headers: { "Accept": "application/json", "Content-Type": "application/json" }
     });
 
     if (response.status == 200) {
         let doctors_cards = document.getElementById("doctor-cards-id");
-        const data = await response.json();
+        const data = await responseDoctors.json();
 
-
+        //Добавление карточек
         for (let i = 0; i < data.length; i++) {
 
             let serviceList = '<ul class="doctor-card__services">'
@@ -26,7 +28,7 @@ async function loadData() {
                 serviceList += `<li class="doctor-card__service">${data[i].Services[j].ServiceName}</li>`
             }
             serviceList += '</ul>'
-
+            //Хардкод
             doctors_cards.innerHTML += `<div class="doctor-card">
                                             <div class="doctor-card__info">
                                                 <h3 class="doctor-card__name">${data[i].Surname} ${data[i].Name} ${data[i].FatherName}</h3>
@@ -45,33 +47,3 @@ async function loadData() {
         }
     }
 }
-
-// Функция для добавления услуг в список
-function addServicesToList(services) {
-    const listElement = document.querySelector(".article__services");
-    services.forEach((service) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = service;
-        listElement.appendChild(listItem);
-    });
-}
-
-//<main class="main">
-//    <section class="doctor-cards">
-//        <div class="doctor-card">
-//            <div class="doctor-card__info">
-//                <h3 class="doctor-card__name">Иванов Иван Иванович</h3>
-//                <p class="doctor-card__detail">Кабинет: 101</p>
-//                <p class="doctor-card__detail">Начало работы: 8:00</p>
-//                <p class="doctor-card__detail">Специализация: Терапевт</p>
-//                <h4 class="doctor-card__services-title">Услуги:</h4>
-//                <ul class="doctor-card__services">
-//                    <li class="doctor-card__service">Консультация</li>
-//                    <li class="doctor-card__service">Лечение</li>
-//                </ul>
-//            </div>
-//            <div class="doctor-card__actions">
-//                <button class="doctor-card__button doctor-card__button--edit">Изменить</button>
-//                <button class="doctor-card__button doctor-card__button--delete">Удалить</button>
-//            </div>
-//        </div>
